61f00d33ef7ff988c3e8171510727797f6830427
abidali05 <mhrabid558@gmail.com>
2023-07-15 14:55:59 +0500

    Laravel stripe connect

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -6,10 +6,7 @@ use App\Services\Stripe\Seller;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Session;
 use Stripe\Account;
-use Stripe\BitcoinTransaction;
 use Stripe\Stripe;
 
 class SellerController extends Controller
